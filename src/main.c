10aaab0bc26e9d70c0ab3625857af7243e5c3258
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-05-08 13:27:37 +0100

    autoupdate

--- src/main.c
+++ src/main.c
@@ -308,14 +308,14 @@ int main ()
   nhaloAHF = read_clueAHFhalos();
   for(ihalo=0;ihalo<nhaloAHF;ihalo++)
     {
-      printf("%llu %f %f %f %f %f %f\n",ihalo,AHFhalo[ihalo].pos[0],AHFhalo[ihalo].pos[1],AHFhalo[ihalo].pos[2],AHFhalo[ihalo].vel[0],AHFhalo[ihalo].vel[1],AHFhalo[ihalo].vel[2]);
+      // printf("%llu %f %f %f %f %f %f\n",ihalo,AHFhalo[ihalo].pos[0],AHFhalo[ihalo].pos[1],AHFhalo[ihalo].pos[2],AHFhalo[ihalo].vel[0],AHFhalo[ihalo].vel[1],AHFhalo[ihalo].vel[2]);
       xb = AHFhalo[ihalo].pos[0]/subsize;
       yb = AHFhalo[ihalo].pos[1]/subsize;
       zb = AHFhalo[ihalo].pos[2]/subsize;
       
       block = xb*nsubperdim*nsubperdim + yb*nsubperdim + zb;
 
-      // printf("%llu/%llu  %d %d %d \n",ihalo,nhaloAHF,xb,yb,zb);
+      printf("%llu/%llu  %d: %d %d %d \n",ihalo,nhaloAHF,block,xb,yb,zb);
       // AHFhalo[ihalo].nextid = hocAHF[block];
       // hocAHF[block] = ihalo;      
     }
