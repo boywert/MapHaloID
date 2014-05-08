e2e0f4ced8b7cccf5329a8d22edc70f13d78a46e
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-05-08 13:20:05 +0100

    autoupdate

--- src/main.c
+++ src/main.c
@@ -304,11 +304,11 @@ int main ()
   /*     FOFhalo[ihalo].nextid = hocFOF[block]; */
   /*     hocFOF[block] = ihalo; */
   /*   } */
-  AHFhalo = malloc(sizeof(struct halostruct));
+  AHFhalo = malloc(0);
   nhaloAHF = read_clueAHFhalos();
   for(ihalo=0;ihalo<nhaloAHF;ihalo++)
     {
-      // printf("%llu %f %f %f %f %f %f\n",ihalo,AHFhalo[ihalo].pos[0],AHFhalo[ihalo].pos[1],AHFhalo[ihalo].pos[2],AHFhalo[ihalo].vel[0],AHFhalo[ihalo].vel[1],AHFhalo[ihalo].vel[2]);
+      printf("%llu %f %f %f %f %f %f\n",ihalo,AHFhalo[ihalo].pos[0],AHFhalo[ihalo].pos[1],AHFhalo[ihalo].pos[2],AHFhalo[ihalo].vel[0],AHFhalo[ihalo].vel[1],AHFhalo[ihalo].vel[2]);
       xb = AHFhalo[ihalo].pos[0]/subsize;
       yb = AHFhalo[ihalo].pos[1]/subsize;
       zb = AHFhalo[ihalo].pos[2]/subsize;
