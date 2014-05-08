f0f3853fbe81e3936759f730eb4cf7050d61584a
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-05-08 10:50:50 +0100

    autoupdate

--- src/main.c
+++ src/main.c
@@ -309,9 +309,9 @@ int main ()
   for(ihalo=0;ihalo<nhaloAHF;ihalo++)
     {
       // printf("%llu %f %f %f %f %f %f\n",ihalo,AHFhalo[ihalo].pos[0],AHFhalo[ihalo].pos[1],AHFhalo[ihalo].pos[2],AHFhalo[ihalo].vel[0],AHFhalo[ihalo].vel[1],AHFhalo[ihalo].vel[2]);
-      xb = AHFhalo[ihalo].pos[0]/subsize;
-      yb = AHFhalo[ihalo].pos[1]/subsize;
-      zb = AHFhalo[ihalo].pos[2]/subsize;
+      xb = 0; // AHFhalo[ihalo].pos[0]/subsize;
+      yb = 0; // AHFhalo[ihalo].pos[1]/subsize;
+      zb = 0; // AHFhalo[ihalo].pos[2]/subsize;
       
       block = xb*nsubperdim*nsubperdim + yb*nsubperdim + zb;
 
