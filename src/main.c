f20673233170c657363ea40a489de1ea37e50253
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-05-10 13:18:04 +0100

    autoupdate

--- src/main.c
+++ src/main.c
@@ -555,7 +555,7 @@ int main (int argc, char** argv)
 	    }
 	  if(maxmerit > 0.)
 	    {
-	      if(rank==0) printf("maxmerit:%f   m:%f D:%f,%f,%f V:%f,%f,%f\n",maxmerit,FOFhalo[maxmeritid].mass-AHFhalo[curhalo_src].mass
+	      if(rank==0) printf("maxmerit:%f   m:%f D:%f,%f,%f V:%f,%f,%f\n",maxmerit,(FOFhalo[maxmeritid].mass-AHFhalo[curhalo_src].mass)/AHFhalo[curhalo_src].mass
 				 ,((FOFhalo[maxmeritid].pos[0] - AHFhalo[curhalo_src].pos[0]))
 				 ,((FOFhalo[maxmeritid].pos[1] - AHFhalo[curhalo_src].pos[1]))
 				 ,((FOFhalo[maxmeritid].pos[2] - AHFhalo[curhalo_src].pos[2]))
