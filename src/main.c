1718d0518ca0650d1bb91f5bada3bc7ec508a750
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-05-10 09:42:11 +0100

    autoupdate

--- src/main.c
+++ src/main.c
@@ -518,7 +518,7 @@ int main (int argc, char** argv)
       while(curhalo_src > -1)
 	{
 	  if(rank==0) printf("Starting: AHF: %d\n",curhalo_src);
-	  sigma_pos = 500.0;
+	  sigma_pos = 1000.0;
 	  sigma_vel = 50.0;
 	  sigma_mass = AHFpmass;
 	  maxmerit = -1.;
